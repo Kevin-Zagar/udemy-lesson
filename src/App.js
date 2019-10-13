88127f3bfd10844de042dafb976c71999bd2033e
kevin <kevin.zagar@yahoo.de>
2019-10-13 20:13:39 +0200

    Changes in the Button and CSS files. Bug fixes.

--- src/App.js
+++ src/App.js
@@ -1,26 +1,20 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react';
 import './App.css';
+import Button from "./Button_Component/Button.js";
+import Panel from "./Panel_Component/Panel.js";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+    render() {
+        return (
+            <div className="App">
+                <div className="App-header">
+                    <h2>Welcome to React</h2>
+                    <Button label="Klick mich"></Button>
+                </div>
+                
+            </div>
+        );
+    }
 }
 
 export default App;
