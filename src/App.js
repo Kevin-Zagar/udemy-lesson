e17bf35b0a762e607450f3ad6c70eba90a3a4b18
Kevin-Zagar <43926874+Kevin-Zagar@users.noreply.github.com>
2019-10-15 13:17:59 +0200

    Panel created.

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, {Component} from 'react';
+
 import './App.css';
 import Button from "./Button_Component/Button.js";
 import Panel from "./Panel_Component/Panel.js";
@@ -11,7 +12,12 @@ class App extends Component {
                     <h2>Welcome to React</h2>
                     <Button label="Klick mich"></Button>
                 </div>
-                
+                <br></br>
+                <Panel title="Überschrift" content="Test"></Panel>
+                <br></br>
+                <Panel title="Noch eine">
+                    <Button label="Klick mich"></Button>
+                </Panel>
             </div>
         );
     }
